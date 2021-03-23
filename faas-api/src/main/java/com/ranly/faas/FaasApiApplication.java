e33f970bf557eac4de22d770ad9962d0c216bae1
三水 <jianmiao>
2021-03-23 20:42:25 +0800

    fix

---
+++ faas-api/src/main/java/com/ranly/faas/FaasApiApplication.java
@@ -0,0 +1,13 @@
+package com.ranly.faas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaasApiApplication.class, args);
+    }
+
+}
---
+++ faas-api/src/test/java/com/ranly/faas/FaasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranly.faas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaasApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
